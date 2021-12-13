a669d120b46898315dd952fd6c636093c1de2b60
hkvijay54 <hkvijay54@gmail.com>
2021-12-13 18:20:59 +0530

    First Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Program 1");
+            Student.PrintStudentName("dhanya");
+        }
+    }
+}
